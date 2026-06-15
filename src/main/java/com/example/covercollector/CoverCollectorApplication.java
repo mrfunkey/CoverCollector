21d12ca11040ffdfe3ff28774ea256d8e88033bc
funkey <165349763+mrfunkey@users.noreply.github.com>
2026-06-15 12:45:43 -0400

    Add files via upload

---
+++ src/main/java/com/example/covercollector/CoverCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.covercollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoverCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/covercollector/CoverCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covercollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
